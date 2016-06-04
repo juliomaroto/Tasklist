e9514ef0e02586b7aa04594720da41397ff59dcb
juliomaroto <juliomaroto96@gmail.com>
2016-06-04 22:38:03 +0200

    flash remove

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
-app.use(flash());
 app.use(cookieParser());
 app.use(session({
       secret: 'OZhCLfxlGp9TtzSXMJtq',
